f22cec1c9c6612c45b009de80b61d436514ba263
lightningkay <ew_34@sina.com>
2021-02-02 22:49:54 +0800

    fix experiment setting

--- single_analyse.sh
+++ single_analyse.sh
@@ -8,7 +8,7 @@ set -e
 
 for m in 0.0 0.1 0.2 0.3
 do
-    for i in {100..500..50}
+    for i in {100..501..50}
     do
         fail=$(echo "${m}*${i}/1"|bc)
         java -cp aggprotocol/target/aggprotocol-1.0-SNAPSHOT-jar-with-dependencies.jar edu.bjut.aggprotocol.app.AggApp -u $i -f $fail -g 10000 >> log/${m}_${i}_10000.log
